56b29b476caaedbf4a3280d5e06bb666d5444062
mmtakeuchi <mikemtaki@gmail.com>
2021-09-07 09:29:58 -0700

    created user post header

--- src/components/UserPost/UserPost.components.jsx
+++ src/components/UserPost/UserPost.components.jsx
@@ -3,7 +3,18 @@ import './UserPost.styles.scss';
 
 const UserPost = () => (
   <div className="user-post">
-    USERPOST
+    <div className="postHeader">
+      <img src="https://source.unsplash.com/nLvqquwiqWQ" alt="user-avatar" className="avatar" />
+      <div className="userInfo">
+        <div className="userName">Batman</div>
+        <div>
+          {`Today at ${new Date().toLocaleTimeString([], { timeStyle: 'short' })}`}
+        </div>
+      </div>
+      <button type="button" className="moreButton">...</button>
+    </div>
+    <div className="postContent">Content</div>
+    <div className="postFooter">Footer</div>
   </div>
 );
 
