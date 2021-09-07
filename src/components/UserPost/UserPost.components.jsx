a32fa3e01e0151bdefd3335e900a26009e9fed79
mmtakeuchi <mikemtaki@gmail.com>
2021-09-07 11:45:06 -0700

    styled post content section

--- src/components/UserPost/UserPost.components.jsx
+++ src/components/UserPost/UserPost.components.jsx
@@ -13,7 +13,7 @@ const UserPost = () => (
       </div>
       <button type="button" className="moreButton">...</button>
     </div>
-    <div className="postContent">Content</div>
+    <div className="postContent">&quot;People need dramatic examples to shake them out of apathy and I can&apos;t do that as Bruce Wayne. As a man, I&apos;m flesh and blood, I can be ignored, I can be destroyed; but as a symbol... as a symbol I can be incorruptible, I can be everlasting.&quot;</div>
     <div className="postFooter">Footer</div>
   </div>
 );
