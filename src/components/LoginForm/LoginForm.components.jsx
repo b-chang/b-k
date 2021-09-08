32bc2ab1bb5f5b1265f3f7965acf5dc3c11e300a
mmtakeuchi <mikemtaki@gmail.com>
2021-09-08 12:59:19 -0700

    write up basic form with controlled inputs

--- src/components/LoginForm/LoginForm.components.jsx
+++ src/components/LoginForm/LoginForm.components.jsx
@@ -1,10 +1,39 @@
 import React, { useState } from 'react';
+import './LoginForm.styles.scss';
 
 const LoginForm = () => {
-  const [data, setData] = useState({});
+  const [values, setValues] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleInputChange = (e) => {
+    setValues({ ...values, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setValues({
+      username: '',
+      password: ''
+    });
+  };
 
   return (
-    <div>LoginForm</div>
+    <div>
+      <h1>LoginForm</h1>
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label htmlFor="username">
+          Username
+          <input type="text" value={values.username} onChange={handleInputChange} id="username" />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input type="text" value={values.password} onChange={handleInputChange} id="password" />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </div>
   );
 };
 
