96845695908ccc6fb74b1bc88bde6b88168f7195
mmtakeuchi <mikemtaki@gmail.com>
2021-09-08 12:06:04 -0700

    create login form component

--- src/components/Home/Home.components.jsx
+++ src/components/Home/Home.components.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Home.styles.scss';
 import Navbar from '../NavBar/NavBar.components';
+import LoginForm from '../LoginForm/LoginForm.components';
 import Feed from '../Feed/Feed.components';
 
 const Home = () => (
   <div className="home">
     <Navbar />
+    <LoginForm />
     <Feed />
   </div>
 );
---
+++ src/components/LoginForm/LoginForm.components.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+  const [data, setData] = useState({});
+
+  return (
+    <div>LoginForm</div>
+  );
+};
+
+export default LoginForm;
