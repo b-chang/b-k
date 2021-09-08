de780e27b04da7fda55ce45ca3f028a1d9a4fe47
mmtakeuchi <mikemtaki@gmail.com>
2021-09-08 15:04:14 -0700

    wrote validation for username and password inputs

--- src/components/LoginForm/LoginForm.components.jsx
+++ src/components/LoginForm/LoginForm.components.jsx
@@ -4,34 +4,66 @@ import './LoginForm.styles.scss';
 const LoginForm = () => {
   const [values, setValues] = useState({
     username: '',
-    password: ''
+    password: '',
+    usernameError: '',
+    passwordError: ''
   });
 
   const handleInputChange = (e) => {
     setValues({ ...values, [e.target.id]: e.target.value });
   };
 
+  const validateInputs = () => {
+    console.log(values);
+    let usernameError = '';
+    let passwordError = '';
+    let isValid = true;
+
+    if (!values.username) {
+      usernameError = 'Enter your username.';
+    }
+
+    if (!values.password) {
+      passwordError = 'Enter your password.';
+    }
+
+    console.log(usernameError, passwordError);
+    if (usernameError || passwordError) {
+      setValues({ ...values, usernameError, passwordError });
+      isValid = false;
+    }
+    console.log(isValid);
+    return isValid;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    setValues({
-      username: '',
-      password: ''
-    });
+    if (validateInputs()) {
+      console.log('success', values);
+      setValues({
+        username: '',
+        password: ''
+      });
+    } else {
+      console.log(values);
+    }
   };
 
   return (
-    <div>
+    <div className="loginContainer">
       <h1>LoginForm</h1>
       <form className="loginForm" onSubmit={handleSubmit}>
         <label htmlFor="username">
-          Username
-          <input type="text" value={values.username} onChange={handleInputChange} id="username" />
+          <span className="inputLabel">Username</span>
+          <input type="text" value={values.username} onChange={handleInputChange} id="username" name="username" />
+          {values.usernameError && <div className="error">{values.usernameError}</div>}
         </label>
         <label htmlFor="password">
-          Password
+          <span className="inputLabel">Password</span>
           <input type="text" value={values.password} onChange={handleInputChange} id="password" />
+          {values.passwordError && <div className="error">{values.passwordError}</div>}
         </label>
-        <button type="submit">Login</button>
+        <button type="submit" className="submitBtn">Login</button>
       </form>
     </div>
   );
